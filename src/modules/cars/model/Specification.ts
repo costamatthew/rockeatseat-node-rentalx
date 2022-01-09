bb0abc6ad0637caec9c5898156b19ffdf9028b0b
Mateus Costa <mateus.costa@roit.com.br>
2022-01-09 20:07:39 -0300

    fix: correct specification name

--- src/modules/cars/model/Specification.ts
+++ src/modules/cars/model/Specification.ts
@@ -1,6 +1,6 @@
 import { v4 as uuidV4 } from 'uuid'
 
-class Specialization {
+class Specification {
     id?: string
     name: string
     description: string
@@ -13,4 +13,4 @@ class Specialization {
     }
 }
 
-export { Specialization }
+export { Specification }
