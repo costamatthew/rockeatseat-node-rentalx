2e3d721e1f8fe3709fbbd573ab61e16c06da74db
Mateus Costa <mateus.costa@roit.com.br>
2022-01-13 22:30:09 -0300

    feat: create upload files

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,27 @@
-import { Router } from "express"
+import { Request, Response, Router } from "express"
+import multer from 'multer'
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory"
 import { listCategoriesController } from "../modules/cars/useCases/listCategories"
 
 const categoriesRoutes = Router()
 
-categoriesRoutes.post('/', (request, response) => {
+const upload = multer({
+    dest: './temp',
+})
+
+categoriesRoutes.post('/', (request: Request, response: Response) => {
     return createCategoryController.handle(request, response)
 })
 
-categoriesRoutes.get('/', (request, response) => {
+categoriesRoutes.get('/', (request: Request, response: Response) => {
     return listCategoriesController.handle(request, response)
+})
+
+categoriesRoutes.post('/import', upload.single('file'), (request: Request, response: Response) => {
+    const { file } = request
+    console.log(file)
+    return response.send()
 })
 
 export { categoriesRoutes }
