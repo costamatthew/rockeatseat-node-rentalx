a30e47bcea9f10553cb4e619321622cdf0077c41
Mateus Costa <mateus.costa@roit.com.br>
2022-01-23 14:21:53 -0300

    feat: create read stream

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,7 +1,20 @@
+import fs from 'fs'
+import csvParser from 'csv-parser'
+
 class ImportCategoryUseCase {
-    execute(file: any) {
-        console.log(file)
+
+    execute(file: any): void {
+        const stream = fs.createReadStream(file.path)
+
+        const parseFile = csvParser()
+
+        stream.pipe(parseFile)
+
+        parseFile.on('data', async (line) => {
+            console.log(line)
+        })
     }
+
 }
 
 export { ImportCategoryUseCase }
