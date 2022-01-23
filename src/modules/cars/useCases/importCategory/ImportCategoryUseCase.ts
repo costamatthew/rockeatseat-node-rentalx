99bb4370876233c9a1e02c580059494fcffdf04c
Mateus Costa <mateus.costa@roit.com.br>
2022-01-23 14:52:33 -0300

    feat: reading upload data

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,18 +1,39 @@
 import fs from 'fs'
-import csvParser from 'csv-parser'
+import { parse } from 'csv-parse';
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository'
+
+interface IimportCategory {
+    name: string
+    description: string
+}
 
 class ImportCategoryUseCase {
 
-    execute(file: any): void {
-        const stream = fs.createReadStream(file.path)
+    constructor(private categoriesRepository: ICategoriesRepository) { }
 
-        const parseFile = csvParser()
+    loadCategories(file: any): Promise<IimportCategory[]> {
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file.path)
+            const categories: IimportCategory[] = []
 
-        stream.pipe(parseFile)
+            const parseFile = parse()
 
-        parseFile.on('data', async (line) => {
-            console.log(line)
+            stream.pipe(parseFile)
+
+            parseFile.on('data', async (line) => {
+                const [name, description] = line
+                categories.push({ name, description })
+            }).on('end', () => {
+                resolve(categories)
+            }).on('error', (err) => {
+                reject(err)
+            })
         })
+    }
+
+    async execute(file: any): Promise<void> {
+        const categories = await this.loadCategories(file)
+        console.log(categories)
     }
 
 }
