c3297d889dce361008c5d381fff33930667ed9a5
Mateus Costa <mateusdsilvacosta@outlook.com>
2022-05-05 20:52:53 -0300

    fix: drop username column

--- src/modules/accounts/UseCases/CreateUser/CreateUserUseCase.ts
+++ src/modules/accounts/UseCases/CreateUser/CreateUserUseCase.ts
@@ -12,11 +12,10 @@ class CreateUserUseCase {
     ) {}
 
     async execute(data: ICreateUserDTO): Promise<void> {
-        const { name, username, email, password, driver_license } = data
+        const { name, email, password, driver_license } = data
 
         await this.usersRepository.create({
             name,
-            username,
             email,
             password,
             driver_license
