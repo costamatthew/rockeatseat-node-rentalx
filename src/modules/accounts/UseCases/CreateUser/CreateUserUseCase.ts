f5ec7d50f5ac516180b5e9ddd96f95ec86281563
Mateus Costa <mateusdsilvacosta@outlook.com>
2022-05-05 22:25:05 -0300

    feat: check if the email already exists

--- src/modules/accounts/UseCases/CreateUser/CreateUserUseCase.ts
+++ src/modules/accounts/UseCases/CreateUser/CreateUserUseCase.ts
@@ -15,6 +15,12 @@ class CreateUserUseCase {
     async execute(data: ICreateUserDTO): Promise<void> {
         const { name, email, password, driver_license } = data
 
+        const userAlreadyExists = await this.usersRepository.findByEmail(email)
+
+        if (userAlreadyExists) {
+            throw new Error('User already exists')
+        }
+
         const passwordHash = await hash(password, 8)
 
         await this.usersRepository.create({
