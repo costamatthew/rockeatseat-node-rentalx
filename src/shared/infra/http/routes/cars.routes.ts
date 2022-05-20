6588793492cf0b47cf268aed74a13f6e9c339c29
Mateus Costa <mateusdsilvacosta@outlook.com>
2022-05-19 22:31:22 -0300

    feat: create list available cars

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -2,11 +2,14 @@ import { Router } from 'express'
 
 import { ensureAdmin } from '../middlewares/ensureAdmin'
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
+
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController'
+import { ListAvailableCarsController } from '@modules/cars/useCases/listAvaliableCars/ListAvailableCarsController'
 
 const carsRoutes = Router()
 
 const createCategoryController = new CreateCarController()
+const listAvailableCarsUseCase = new ListAvailableCarsController()
 
 carsRoutes.post(
     '/',
@@ -14,5 +17,7 @@ carsRoutes.post(
     ensureAdmin,
     createCategoryController.handle
 )
+
+carsRoutes.get('/avaliable', listAvailableCarsUseCase.handle)
 
 export { carsRoutes }
