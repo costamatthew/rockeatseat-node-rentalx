391ab6ece2d365dfd7866309af8302ddbb9fd0d7
Mateus Costa <mateusdsilvacosta@outlook.com>
2022-05-24 21:21:21 -0300

    feat: add relation with Specification on Car entity

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,7 +1,8 @@
-import { Entity, PrimaryColumn, Column, CreateDateColumn, JoinColumn, ManyToOne } from 'typeorm'
+import { Entity, PrimaryColumn, Column, CreateDateColumn, JoinColumn, ManyToOne, ManyToMany, JoinTable } from 'typeorm'
 import { v4 as uuidV4 } from 'uuid'
 
 import { Category } from './Category'
+import { Specification } from './Specification'
 
 @Entity('cars')
 class Car {
@@ -36,6 +37,14 @@ class Car {
 
     @Column()
     category_id: string
+
+    @ManyToMany(() => Specification)
+    @JoinTable({
+        name: 'specifications_cars',
+        joinColumns: [{ name: 'car_id' }],
+        inverseJoinColumns: [{ name: 'specification_id'}],
+    })
+    specifications: Specification[]
 
     @CreateDateColumn()
     created_at: Date
