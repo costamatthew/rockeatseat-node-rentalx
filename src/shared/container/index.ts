1eb2490a462bd632f998466c15a44ea32546e859
Mateus Costa <mateusdsilvacosta@outlook.com>
2022-05-27 01:28:39 -0300

    feat: create DateProvider

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,10 +1,15 @@
 import { container } from 'tsyringe'
 
-import { ICategoriesRepository } from '@modules/cars/repositories/'
-import { ISpecificationsRepository } from '@modules/cars/repositories/'
+import '@shared/container/providers'
 
-import { 
-    CarsRepository, CategoriesRepository, SpecificationsRepository, CarsImagesRepository
+import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository'
+import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository'
+
+import {
+    CarsRepository,
+    CategoriesRepository,
+    SpecificationsRepository,
+    CarsImagesRepository
 } from '@modules/cars/infra/typeorm/repositories'
 
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository'
@@ -12,28 +17,24 @@ import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/Us
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository'
 import { ICarsImagesRepository } from '@modules/cars/repositories/ICarsImagesRepository'
 
-
 container.registerSingleton<ICategoriesRepository>(
-    "CategoriesRepository",
+    'CategoriesRepository',
     CategoriesRepository
 )
 
 container.registerSingleton<ISpecificationsRepository>(
-    "SpecificationsRepository",
+    'SpecificationsRepository',
     SpecificationsRepository
 )
 
 container.registerSingleton<IUsersRepository>(
-    "UsersRepository",
+    'UsersRepository',
     UsersRepository
 )
 
-container.registerSingleton<ICarsRepository>(
-    "CarsRepository",
-    CarsRepository
-)
+container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository)
 
 container.registerSingleton<ICarsImagesRepository>(
-    "CarsImagesRepository",
+    'CarsImagesRepository',
     CarsImagesRepository
 )
