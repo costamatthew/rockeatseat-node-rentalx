7655586d80d3b75c87d6c69a3ab304473c5ac47e
Mateus Costa <mateusdsilvacosta@outlook.com>
2022-07-07 20:22:15 -0300

    refactor ensureAuthenticated

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,8 +1,8 @@
-import { Request, Response, NextFunction } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import { verify } from 'jsonwebtoken'
 
 import { AppError } from '@shared/errors/AppError'
-import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UserRepository'
+import auth from '@config/auth'
 
 interface IPayload {
     sub: string
@@ -22,15 +22,7 @@ export async function ensureAuthenticated(
     const [, token] = authHeader.split(' ')
 
     try {
-        const { sub: user_id } = verify(token, 'chaveSecreta') as IPayload
-
-        const usersRepository = new UsersRepository()
-
-        const user = await usersRepository.findById(user_id)
-
-        if (!user) {
-            throw new AppError('User does not exists!', 401)
-        }
+        const { sub: user_id } = verify(token, auth.secret_token) as IPayload
 
         request.user = {
             id: user_id
