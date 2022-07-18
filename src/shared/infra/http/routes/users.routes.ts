bfe4de1c638992a94033600a647df8264df734a3
Mateus Costa <mateusdsilvacosta@outlook.com>
2022-07-18 16:27:00 -0300

    feat: create service to return ProfileUser

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -6,6 +6,7 @@ import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
 
 import { CreateUserController } from '@modules/accounts/UseCases/CreateUser/CreateUserController'
 import { UpdateUserAvatarController } from '@modules/accounts/UseCases/UpdateUserAvatar/UpdateUserAvatarController'
+import { ProfileUserController } from '@modules/accounts/UseCases/profileUserUseCase/ProfileUserController'
 
 const usersRouters = Router()
 
@@ -13,6 +14,7 @@ const uploadAvatar = multer(uploadConfig)
 
 const createUserController = new CreateUserController()
 const updateUserAvatarController = new UpdateUserAvatarController()
+const profileUserController = new ProfileUserController()
 
 usersRouters.post('/', createUserController.handle)
 
@@ -22,5 +24,7 @@ usersRouters.patch(
     uploadAvatar.single('avatar'),
     updateUserAvatarController.handle
 )
+
+usersRouters.get('/profile', ensureAuthenticated, profileUserController.handle)
 
 export { usersRouters }
