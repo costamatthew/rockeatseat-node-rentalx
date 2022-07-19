e5288ea24ca6764a7588a55387e9f14cbd9d56cb
Mateus Costa <mateusdsilvacosta@outlook.com>
2022-07-19 20:34:10 -0300

    fix: database connection

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,15 +1,14 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm'
 
-interface IOptions {
-    host: string
-}
-
-export default async (host = 'database'): Promise<Connection> => {
+export default async (): Promise<Connection> => {
     const defaultOptions = await getConnectionOptions()
 
     return createConnection(
         Object.assign(defaultOptions, {
-            host //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+            database:
+                process.env.NODE_ENV === 'test'
+                    ? 'rentx_test'
+                    : defaultOptions.database
         })
     )
 }
